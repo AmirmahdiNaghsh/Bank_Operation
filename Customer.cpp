424f7b2f125be94a3d8aa1091125194f772f730d
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 00:12:50 -0700

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,6 +1,10 @@
 #include "Customer.h"
 #include "Account.h" 
 #include <iostream>
+using namespace std;
+
+Customer::Customer(const string& fname, const string& lname, const string& natId, int userAge, const string& uname, const string& pass)
+    : User(fname, lname, natId, userAge, uname, pass) {}
 
 void Customer::displayInfo() const {
     User::displayInfo(); 
@@ -10,5 +14,13 @@ void Customer::displayInfo() const {
         cout << "This customer has no bank accounts." << endl;
     } else {
         bankAccounts.display(); 
+    }
+}
+
+Customer::~Customer() {
+    auto* currentNode = bankAccounts.getHead();
+    while (currentNode != nullptr) {
+        delete currentNode->getData();
+        currentNode = currentNode->getNext();
     }
 }
