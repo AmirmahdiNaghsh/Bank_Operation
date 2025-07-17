3bea649179437ea8f3ea1fdb8fcbc22ff321ec3f
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-16 23:59:13 -0700

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,59 +1,89 @@
 #include "Account.h"
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
-Account::Account(const string& accNum, const string & cardNum, const string& sheba, const string& pin,const string& pin2)
-: accountNumber(accNum),
-cardNumber(cardNum),
-shebaNumber(sheba),
-pin1(pin),
-pin2_static(pin2),
-balance(0.0),
-cvv2(to_string(rand()% 900 + 100)),
-pin2_dynamic("")
-{
+using namespace std;
+
+Account::Account(const string& accNum, const string& cardNum, const string& sheba, const string& pin, const string& pin2)
+    : accountNumber(accNum),
+      cardNumber(cardNum),
+      shebaNumber(sheba),
+      pin1(pin),
+      pin2_static(pin2),
+      balance(0.0),
+      cvv2(to_string(rand() % 900 + 100)),
+      pin2_dynamic("") {
     time_t now = time(0);
-    tm *ltm - localtime(&now);
+    tm *ltm = localtime(&now); 
     ltm->tm_year += 5;
     this->expirationDate = mktime(ltm);
 }
 
-Account::~Account(){}
+Account::~Account() {}
 
-string Account::getCardNumber() const{
+string Account::getCardNumber() const {
     return cardNumber;
 }
 
-string Account::getAccountNumber() const{
+string Account::getAccountNumber() const {
     return accountNumber;
 }
 
-string Account::getBalance() const{
+double Account::getBalance() const {
     return balance;
 }
 
-bool Account::isExpired() const{
+bool Account::isExpired() const {
     return time(0) > expirationDate;
-
-    void Account::setDynamicpin(const sting& newDynamicPin) {
-        this->pin2_dynamic = newDynamicPin; 
-    } 
 }
 
-void Account::deposit(double amount){
-    if (amount > 0)
-    {
+void Account::setDynamicPin(const string& newDynamicPin) {
+    this->pin2_dynamic = newDynamicPin;
+}
+
+void Account::deposit(double amount) {
+    if (amount > 0) {
         balance += amount;
     }
 }
 
-void Account::withdraw(double amount){
-    if (amount > 0 && balance>= amount)
-    {
+bool Account::withdraw(double amount) {
+    if (amount > 0 && balance >= amount) {
         balance -= amount;
         return true;
     }
     return false;
-    
+}
+
+bool Account::checkPin1(const string& p) const {
+    return pin1 == p;
+}
+
+bool Account::checkPin2Static(const string& p) const {
+    return pin2_static == p;
+}
+
+bool Account::checkPin2Dynamic(const string& p) const {
+    if (!pin2_dynamic.empty() && pin2_dynamic == p) {
+        return true;
+    }
+    return false;
+}
+
+void Account::changePins(const string& newPin1, const string& newPin2) {
+    this->pin1 = newPin1;
+    this->pin2_static = newPin2;
+}
+
+void Account::displayInfo() const {
+    cout << "Account Number: " << accountNumber << endl;
+    cout << "Card Number: " << cardNumber << endl;
+    cout << "Sheba: " << shebaNumber << endl;
+    cout << "Balance: " << balance << " Toman" << endl;
+// Start Gpt codes
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d", localtime(&expirationDate));
+    cout << "Expiration Date: " << buffer << endl;
+// end Gpt codes
 }
