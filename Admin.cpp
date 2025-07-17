4925ac6f8ebc3753a407b646d063bb6fbf9b6db9
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 12:03:43 -0700

    DEBUG

--- Admin.cpp
+++ Admin.cpp
@@ -1,54 +1,45 @@
 #include "Admin.h"
 #include "Customer.h"
-
 #include "SavingsAccount.h"
 #include "CurrentAccount.h"
 #include "LoanAccount.h"
-
 #include <iostream>
 
 using namespace std;
-
 Admin::Admin(const string& fname, const string& lname, const string& natId, int userAge, const string& uname, const string& pass)
     : User(fname, lname, natId, userAge, uname, pass) {}
-
 void Admin::viewAllUsers(const LinkedList<User*>& users) const {
     cout << "--- System Users List ---" << endl;
     users.display();
 }
-
 void Admin::addCustomer(LinkedList<User*>& users, const string& fname, const string& lname, const string& natId, int age, const string& uname, const string& pass) {
-    if (usres.find([&](users* u){return u->getUsername() == uname;}))
-    {
-        cout<< "Error: Username " << uname << " already exists."<< endl;
-        return;
-
-        Customer* newCustomer = new Customer(fname, lname,natId,age,uname,pass);
-        users.add(newCustomer);
-        cout<< " customer " << uname << " added successfully." << endl;
-    }
-
-bool Admin::removeCustomer(LinkedList<User*>& users, const string username){
-    User* userToRemove = users.find([&](user* u){retuen u->getUsername() == username;})
-
-    if (userToRemove == nullptr)
-    {
-        cout << "Error: User " << username << " not found." << endl;
-        return false;
-    }
     
+    if (users.find([&](User* u){ return u->getUsername() == uname; })) {
+        cout << "Error: Username '" << uname << "' already exists." << endl;
+        return;
+    }
+    Customer* newCustomer = new Customer(fname, lname, natId, age, uname, pass);
+    users.add(newCustomer);
+    cout << "Customer '" << uname << "' added successfully." << endl;
 }
 
-if (dynamic_cast<Customer*>(userToRemove))
-{
-    users.remove([&](User* u){return u->getUsername() == username;});
+bool Admin::removeCustomer(LinkedList<User*>& users, const string& username) { 
+    User* userToRemove = users.find([&](User* u){ return u->getUsername() == username; });
 
-    delete userToRemove;
-    cout<< "customer " << username << " removed Successfully." << endl;
-    return true;
-}else{
-    cout<< "Error: Admins cannot be removed." << endl;
-    return false;
+    if (userToRemove == nullptr) {
+        cout << "Error: User '" << username << "' not found." << endl;
+        return false;
+    }
+
+    if (dynamic_cast<Customer*>(userToRemove)) {
+        users.remove([&](User* u){ return u->getUsername() == username; });
+        delete userToRemove;
+        cout << "Customer '" << username << "' removed successfully." << endl;
+        return true;
+    } else {
+        cout << "Error: Admins cannot be removed." << endl;
+        return false;
+    }
 }
 
 void Admin::addAccountToCustomer(Customer* customer, int accountType, const string& accNum, const string& cardNum, const string& sheba, const string& pin, const string& pin2) {
--- Admin.h
+++ Admin.h
@@ -13,9 +13,9 @@ public:
     void viewAllUsers(const LinkedList<User*>& users) const;
     void addCustomer(LinkedList<User*>& users, const string& fname, const string& lname, const string& natId, int age, const string& uname, const string& pass);
     bool removeCustomer(LinkedList<User*>& users, const string& username);
-    void addAccountToCustomer(Customer* customer, const string& accNum, const string& cardNum, const string& sheba, const string& pin, const string& pin2);
+    void addAccountToCustomer(Customer *customer, int accountType, const string &accNum, const string &cardNum, const string &sheba, const string &pin, const string &pin2);
+    void addAccountToCustomer(Customer *customer, const string &accNum, const string &cardNum, const string &sheba, const string &pin, const string &pin2);
     void changeUserInfo(User* userToChange, const string& newFirstName, const string& newLastName, int newAge);
 
 };
-
 #endif
