52145b51be50acac296e73445b28bc0858b71bdf
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 00:02:12 -0700

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -3,3 +3,25 @@
 
 #include "User.h"
 #include "LinkedList.h"
+
+class Account;
+
+class Customer : public User {
+private:
+    LinkedList<Account*> bankAccounts; 
+
+public:
+
+    Customer(const string& fname, const string& lname, const string& natId, int userAge, const string& uname, const string& pass)
+    ~Customer();
+
+    void addAccount(Account* newAccount);
+
+    void displayInfo() const override;
+
+    Account* findAccountByCardNumber(const string& cardNumber);
+
+    LinkedList<Account*>& getAccounts();
+};
+
+#endif
