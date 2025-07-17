b0ddef559a57bdf24662daac16a09472adca9319
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 00:33:18 -0700

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -3,3 +3,19 @@
 
 #include "User.h"
 #include "LinkedList.h"
+
+class Customer;
+
+class Admin : public User {
+public:
+
+    Admin(const string& fname, const string& lname, const string& natId, int userAge, const string& uname, const string& pass);
+    void viewAllUsers(const LinkedList<User*>& users) const;
+    void addCustomer(LinkedList<User*>& users, const string& fname, const string& lname, const string& natId, int age, const string& uname, const string& pass);
+    bool removeCustomer(LinkedList<User*>& users, const string& username);
+    void addAccountToCustomer(Customer* customer, const string& accNum, const string& cardNum, const string& sheba, const string& pin, const string& pin2);
+    void changeUserInfo(User* userToChange, const string& newFirstName, const string& newLastName, int newAge);
+
+};
+
+#endif
