5c72017cdc1e578dca403b5222dbd1b96a92c6b3
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 00:17:49 -0700

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -33,3 +33,14 @@ void Customer::addAccount(Account* newAccount) {
         delete newAccount;
     }
 }
+
+void Customer::displayInfo() const {
+    User::displayInfo();
+    
+    cout << "--- Bank Accounts ---" << endl;
+    if (bankAccounts.getCount() == 0) {
+        cout << "This customer has no bank accounts." << endl;
+    } else {
+        bankAccounts.display();
+    }
+}
