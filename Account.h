4ec40459c2042ddcf03627cfe6acac399348d30a
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 00:44:02 -0700

    Update Account.h

--- Account.h
+++ Account.h
@@ -2,7 +2,7 @@
 #define ACCOUNT_H
 
 #include <string>
-#include <ctime> 
+#include <ctime>
 
 using namespace std;
 
@@ -12,35 +12,38 @@ protected:
     string shebaNumber;
     string accountNumber;
     string cvv2;
-    double balance; 
+    double balance;
     time_t expirationDate;
     string pin1;
-    string pin2_static; 
+    string pin2_static;
     string pin2_dynamic;
 
 public:
     Account(const string& accNum, const string& cardNum, const string& sheba, const string& pin, const string& pin2);
-
     virtual ~Account();
 
     string getCardNumber() const;
     string getAccountNumber() const;
 
     double getBalance() const;
+
     bool isExpired() const;
 
-    void setDynamicpin(const string& newDynamicpin);
+    void setDynamicPin(const string& newDynamicPin);
 
     void deposit(double amount);
-    bool withdraw(double amount);
-    bool checkPin1(const string & p) const;
-    bool checkPin2Static(const string & p) const;
-    bool checkpin2Dynamic(const string & p) const;
-    void changePins(const string & newPin1, const string& newpin2);
 
-    virtual void displayAccountTypeDifference() const = 0;
+    bool withdraw(double amount);
+
+    bool checkPin1(const string& p) const;
+    bool checkPin2Static(const string& p) const;
+
+    bool checkPin2Dynamic(const string& p) const;
+
+    void changePins(const string& newPin1, const string& newPin2);
 
     virtual void displayInfo() const;
+    virtual void displayAccountTypeDifference() const = 0;
 };
 
-#endif
+#endif 
