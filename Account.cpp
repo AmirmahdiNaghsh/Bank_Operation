0e57a14fc9bb56450754e9a0145041e42b6bd34d
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-16 23:36:23 -0700

    Create Account.cpp

---
+++ Account.cpp
@@ -0,0 +1,20 @@
+#include "Account.h"
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+Account::Account(const string& accNum, const string & cardNum, const string& sheba, const string& pin,const string& pin2)
+: accountNumber(accNum),
+cardNumber(cardNum),
+shebaNumber(sheba),
+pin1(pin),
+pin2_static(pin2),
+balance(0.0),
+cvv2(to_string(rand()% 900 + 100)),
+pin2_dynamic("")
+{
+    time_t now = time(0);
+    tm *ltm - localtime(&now);
+    ltm->tm_year += 5;
+    this->expirationDate = mktime(ltm);
+}
