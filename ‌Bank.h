5b46b04ea0cb07ce046d759609e2c01937d7c237
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 09:52:38 -0700

    Update ‌Bank.h

--- ‌Bank.h
+++ ‌Bank.h
@@ -18,3 +18,20 @@ private:
     User* findUserByUsername(const string& username);
     Account* findAccountByCardNumber(const string& cardNumber);
     
+public:
+    Bank();
+    ~Bank();
+    void run();
+private:
+    void showMainMenu();
+    void handleLogin();
+    void handleLogout();
+    void showAdminMenu(Admin* admin);
+    void showCustomerMenu(Customer* customer);
+    void performCardToCard(Customer* customer);
+    void changeAccountPassword(Customer* customer);
+    void viewCustomerAccounts(Customer* customer);
+    void setupInitialData();
+};
+
+#endif 
