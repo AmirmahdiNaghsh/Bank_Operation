ab63867373aedf16cf5ead358b56e3233d467fdf
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 00:59:34 -0700

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -16,3 +16,11 @@ void Admin::viewAllUsers(const LinkedList<User*>& users) const {
     cout << "--- System Users List ---" << endl;
     users.display();
 }
+
+void Admin::addCustomer(LinkedList<User*>& users, const string& fname, const string& lname, const string& natId, int age, const string& uname, const string& pass) {
+    if (usres.find([&](users* u){return u->getUsername() == uname;}))
+    {
+        cout<< "Error: Username " << uname << " already exists."<< endl;
+        return;
+    }
+
