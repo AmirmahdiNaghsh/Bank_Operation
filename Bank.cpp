d975b0d5c75ac88e37b8a5dd83965e3e11176f42
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-07-17 11:15:37 -0700

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -1,7 +1,14 @@
 #include "Bank.h"
 #include "Admin.h"
 #include "Customer.h"
-#include "SavingsAccount.h"
+#include "SavingAccount.h"
 #include "CurrentAccount.h"
 #include <iostream>
 #include <limits>
+
+using namespace std;
+
+Bank::Bank() : currentUser(nullptr) {
+    srand(time(NULL));
+    cout << "Welcome to the Bank Management System!" << endl;
+}
